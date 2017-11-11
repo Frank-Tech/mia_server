a1201a426cdc8d7147e4c99ec9ff6b567c86a5c3
tzlil <teselil123@gmail.com>
2017-11-11 16:55:01 +0200

    get near users

--- queryHandler.py
+++ queryHandler.py
@@ -21,6 +21,7 @@ class QueryHandler:
             row = {
                 "date_created": datetime.datetime.now(),
                 "mid": json_data["mid"],
+                "face_id": json_data["id"],
                 "email": json_data["email"],
                 "name": json_data["name"],
                 "gender": json_data["gender"],
@@ -46,12 +47,11 @@ class QueryHandler:
         try:
             longitude = self._request.get("lo", "")
             latitude = self._request.get("la", "")
-            user_id = self._request.get("uid", "")
+            email = self._request.get("e", "")
 
             location = {
                 "date_created": datetime.datetime.now(),
                 "date_updated": datetime.datetime.now(),
-                "user_id": user_id,
                 "longitude": longitude,
                 "latitude": latitude,
             }
@@ -60,7 +60,7 @@ class QueryHandler:
             users_table = MySqlHandler.users()
 
             existing_user = select([users_table.c.id]) \
-                .where(users_table.c.id == location["user_id"]) \
+                .where(users_table.c.email == email) \
                 .execute().first()
 
             if existing_user is not None:
@@ -87,32 +87,37 @@ class QueryHandler:
 
     def locations_get(self):
         try:
-            user_id = self._request.get("uid", "")
+            face_id = self._request.get("fid", "")
 
             locations_table = MySqlHandler.locations()
             users_table = MySqlHandler.users()
 
             existing_user = select([users_table.c.id]) \
-                .where(users_table.c.id == user_id) \
+                .where(users_table.c.face_id == face_id) \
                 .execute().first()
 
             if existing_user is not None:
 
                 current_location = select([locations_table.c.longitude,
                                            locations_table.c.latitude]) \
-                    .where(locations_table.c.user_id == user_id) \
+                    .where(locations_table.c.user_id == existing_user[0]) \
                     .execute().first()
 
-                query = ("SELECT *, ( ( ( Acos(Sin(( {0} * Pi() / 180 )) * Sin(( `latitude` * Pi() / 180 ))+ Cos (( {0} * Pi() / 180 )) * Cos(( `latitude` * Pi() / 180 )) * Cos(( ( {1} - `longitude` ) * Pi() / 180 ))) ) * 180 / Pi ()) * 60 * 1.1515 * 1.609344 * 1000 ) AS METERS FROM users_location HAVING  METERS <= 100")\
-                    .format(current_location[1], current_location[0])
+                query = ("SELECT longitude, latitude, user_id, ( ( ( Acos(Sin(( {0} * Pi() / 180 )) * Sin(( `latitude` * Pi() / 180 ))+ Cos (( {0} * Pi() / 180 )) * Cos(( `latitude` * Pi() / 180 )) * Cos(( ( {1} - `longitude` ) * Pi() / 180 ))) ) * 180 / Pi ()) * 60 * 1.1515 * 1.609344 * 1000 ) AS METERS FROM users_location "
+                         "HAVING  METERS <= 20000 and user_id <>{2}")\
+                    .format(current_location[1], current_location[0], existing_user[0])
 
                 sql = text(query)
                 result = MySqlHandler.server.execute(sql)
-                names = []
-                for row in result:
-                    names.append(row[0])
 
-                print names
+                near_users = []
+                for row in result:
+                    row_data = dict(row.items())
+                    row_data["face_id"] = face_id
+                    near_users.append(json.dumps(row_data, default=str))
+
+                return near_users
+
 
         except Exception, e:
             print e
